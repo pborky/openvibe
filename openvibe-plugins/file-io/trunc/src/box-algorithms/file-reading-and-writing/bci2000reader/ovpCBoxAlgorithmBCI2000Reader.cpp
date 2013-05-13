pborky/openvibe

commit e7fe4d23a2abff06f1b399ba2b3b18e06b9927c2

everything: (Time Arithmetics Refactoring I/II)  * Refactored most time->sample and sample->time computations to use the time arithmetics class  * CMake will now include *.hpp file dependencies in the few cases they exist openvibe/ovITimeArithmetics.h:  * Defined some overloads as private to prevent unintended usage  * Changed timeToSeconds() and secondsToTime() computations to be more accurate  * Renamed some functions to better reflect their usage openvibe/test_timearithmetic:  * Rewrote for more clarity, added some more tests
git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe/trunk@3597 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

openvibe-plugins/file-io/trunc/src/box-algorithms/file-reading-and-writing/bci2000reader/ovpCBoxAlgorithmBCI2000Reader.cpp
@@ -1,5 +1,7 @@
 #include "ovpCBoxAlgorithmBCI2000Reader.h"
 
+#include <openvibe/ovITimeArithmetics.h>
+
 #include <iostream>
 #include <sstream>
 #include <cstdio>
@@ -100,7 +102,7 @@ boolean CBoxAlgorithmBCI2000Reader::processClock(IMessageClock& rMessageClock)
 
 uint64 CBoxAlgorithmBCI2000Reader::getClockFrequency(void)
 {
-	return ((uint64)m_ui32Rate<<32)/m_ui32SampleCountPerBuffer;
+	return ITimeArithmetics::sampleCountToTime(m_ui32Rate, m_ui32SampleCountPerBuffer);
 }
 
 void CBoxAlgorithmBCI2000Reader::sendHeader(void)
@@ -140,10 +142,8 @@ boolean CBoxAlgorithmBCI2000Reader::process(void)
 			}
 		}
 		m_oSignalEncoder.encodeBuffer(0);
-		uint64 StartTime;
-		uint64 EndTime;
-		StartTime=(((uint64)(m_ui32SamplesSent))<<32)/m_ui32Rate;
-		EndTime=(((uint64)(m_ui32SamplesSent+m_ui32SampleCountPerBuffer))<<32)/m_ui32Rate;
+		uint64 StartTime = ITimeArithmetics::sampleCountToTime(m_ui32Rate, m_ui32SamplesSent);
+		uint64 EndTime = ITimeArithmetics::sampleCountToTime(m_ui32Rate, m_ui32SamplesSent+m_ui32SampleCountPerBuffer);
 		m_ui32SamplesSent+=l_ui32SamplesRead;
 		if (m_pB2KReaderHelper->getSamplesLeft()==0)
 		{

openvibe-plugins/file-io/trunc/src/ovpCBCICompetitionIIIbReader.cpp
@@ -1,5 +1,7 @@
 #include "ovpCBCICompetitionIIIbReader.h"
 
+#include <openvibe/ovITimeArithmetics.h>
+
 #include <iostream>
 #include <cmath>
 #include <cfloat>
@@ -207,7 +209,7 @@ namespace OpenViBEPlugins
 							"Please consider adjusting the BCI Competition IIIb reader settings to correct this!\n";
 					}
 
-					m_ui64ClockFrequency = ( ((uint64)m_ui32SamplingRate<<32) / (uint64)m_ui32SamplesPerBuffer);
+					m_ui64ClockFrequency = ITimeArithmetics::sampleCountToTime(m_ui32SamplingRate, m_ui32SamplesPerBuffer);
 				}
 
 			}
@@ -335,8 +337,8 @@ namespace OpenViBEPlugins
 			//A signal matrix is ready to be output
 			m_pSignalOutputWriterHelper->writeBuffer(*m_pWriter[0]);
 
-			l_ui64StartTime=(((uint64)(m_ui32SentSampleCount - i))<<32)/m_ui32SamplingRate;
-			l_ui64EndTime  =(((uint64)(m_ui32SentSampleCount))<<32)/m_ui32SamplingRate;
+			l_ui64StartTime = ITimeArithmetics::sampleCountToTime(m_ui32SamplingRate, (uint64)(m_ui32SentSampleCount - i));
+			l_ui64EndTime  =  ITimeArithmetics::sampleCountToTime(m_ui32SamplingRate, (uint64)(m_ui32SentSampleCount));
 
 			l_pBoxIO->markOutputAsReadyToSend(0, l_ui64StartTime, l_ui64EndTime);
 			//////
@@ -422,7 +424,7 @@ namespace OpenViBEPlugins
 				for(size_t j=0 ; j<l_oEvents.size() ; j++)
 				{
 					//compute date
-					l_ui64EventDate = ( ((uint64)l_oEvents[j].second)<<32)/m_ui32SamplingRate;
+					l_ui64EventDate = ITimeArithmetics::sampleCountToTime(m_ui32SamplingRate, l_oEvents[j].second);
 					m_pStimulationOutputWriterHelper->setStimulation(j, l_oEvents[j].first, l_ui64EventDate);
 				}
 
@@ -430,7 +432,7 @@ namespace OpenViBEPlugins
 				if(m_bEndOfFile)
 				{
 					//compute date
-					l_ui64EventDate = (((uint64)m_ui32SentSampleCount)<<32) / m_ui32SamplingRate;
+					l_ui64EventDate = ITimeArithmetics::sampleCountToTime(m_ui32SamplingRate, m_ui32SentSampleCount);
 					m_pStimulationOutputWriterHelper->setStimulation(l_oEvents.size(), 0x3FF, l_ui64EventDate);
 				}
 

openvibe-plugins/file-io/trunc/src/ovpCGDFFileReader.cpp
@@ -1,5 +1,7 @@
 #include "ovpCGDFFileReader.h"
 
+#include <openvibe/ovITimeArithmetics.h>
+
 #include <system/Memory.h>
 #include <cmath>
 
@@ -176,7 +178,8 @@ boolean CGDFFileReader::uninitialize()
 
 boolean CGDFFileReader::processClock(CMessageClock& rMessageClock)
 {
-	if(rMessageClock.getTime() > (((uint64)(m_ui32SentSampleCount+m_pSignalDescription.m_ui32SampleCount))<<32)/m_pSignalDescription.m_ui32SamplingRate)
+	uint64 l_ui64SampleTime = ITimeArithmetics::sampleCountToTime(m_pSignalDescription.m_ui32SamplingRate, m_ui32SentSampleCount+m_pSignalDescription.m_ui32SampleCount);
+	if(rMessageClock.getTime() > l_ui64SampleTime)
 	{
 		getBoxAlgorithmContext()->markAlgorithmAsReadyToProcess();
 	}
@@ -459,7 +462,7 @@ boolean CGDFFileReader::readFileHeader()
 					<< "Buffer size was " << m_ui32SamplesPerBuffer << "\n";
 			}
 
-			m_ui64ClockFrequency = ( ((uint64)m_pSignalDescription.m_ui32SamplingRate<<32) / (uint64)m_ui32SamplesPerBuffer);
+			m_ui64ClockFrequency = ITimeArithmetics::sampleCountToTime(m_pSignalDescription.m_ui32SamplingRate, m_ui32SamplesPerBuffer);
 		}
 
 		//Send the data to the output
@@ -554,7 +557,7 @@ void CGDFFileReader::writeEvents()
 	for(size_t i=0 ; i<m_oEvents.size() ; i++)
 	{
 		//compute date
-		l_ui64EventDate = ( ((uint64)(m_oEvents[i].m_ui32Position)) <<32)/m_pSignalDescription.m_ui32SamplingRate;
+		l_ui64EventDate = ITimeArithmetics::sampleCountToTime(m_pSignalDescription.m_ui32SamplingRate, m_oEvents[i].m_ui32Position);
 		m_pStimulationOutputWriterHelper->setStimulation(i, m_oEvents[i].m_ui16Type, l_ui64EventDate);
 	}
 
@@ -721,9 +724,9 @@ boolean CGDFFileReader::process()
 		//A signal matrix is ready to be output
 		m_pSignalOutputWriterHelper->writeBuffer(*m_pWriter[GDFReader_SignalOutput]);
 
-		l_ui64StartTime=(((uint64)(m_ui32SentSampleCount - m_pSignalDescription.m_ui32SampleCount))<<32)/m_pSignalDescription.m_ui32SamplingRate;
+		l_ui64StartTime= ITimeArithmetics::sampleCountToTime(m_pSignalDescription.m_ui32SamplingRate, (uint64)(m_ui32SentSampleCount - m_pSignalDescription.m_ui32SampleCount));
 
-		l_ui64EndTime  =(((uint64)(m_ui32SentSampleCount))<<32)/m_pSignalDescription.m_ui32SamplingRate;
+		l_ui64EndTime  = ITimeArithmetics::sampleCountToTime(m_pSignalDescription.m_ui32SamplingRate, (uint64)(m_ui32SentSampleCount));
 
 		l_pBoxIO->markOutputAsReadyToSend(GDFReader_SignalOutput, l_ui64StartTime, l_ui64EndTime);
 

openvibe-plugins/samples/trunc/src/ovpCSinusSignalGenerator.cpp
@@ -5,6 +5,8 @@
 #include <cstdlib>
 #include <cstdio>
 
+#include <openvibe/ovITimeArithmetics.h>
+
 using namespace OpenViBE;
 using namespace OpenViBE::Kernel;
 using namespace OpenViBE::Plugins;
@@ -96,7 +98,7 @@ boolean CSinusSignalGenerator::process(void)
 		m_pSignalOutputWriterHelper->writeHeader(*m_pSignalOutputWriter);
 		m_bHeaderSent=true;
 
-		uint64 l_ui64Time=(((uint64)(m_ui32SentSampleCount))<<32)/m_ui32SamplingFrequency;
+		uint64 l_ui64Time=ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, m_ui32SentSampleCount);
 		l_pDynamicBoxContext->markOutputAsReadyToSend(0, l_ui64Time, l_ui64Time);
 	}
 	else
@@ -115,14 +117,18 @@ boolean CSinusSignalGenerator::process(void)
 		m_pSignalOutputWriterHelper->writeBuffer(*m_pSignalOutputWriter);
 		m_ui32SentSampleCount+=m_ui32GeneratedEpochSampleCount;
 
-		uint64 l_ui64StartTime;
-		uint64 l_ui64EndTime;
-		l_ui64StartTime=(((uint64)(l_ui32SentSampleCount))<<32)/m_ui32SamplingFrequency;
-		l_ui64EndTime  =(((uint64)(m_ui32SentSampleCount))<<32)/m_ui32SamplingFrequency;
+		uint64 l_ui64StartTime = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, l_ui32SentSampleCount);
+		uint64 l_ui64EndTime = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, m_ui32SentSampleCount);
+
 		l_pDynamicBoxContext->markOutputAsReadyToSend(0, l_ui64StartTime, l_ui64EndTime);
 	}
 
 	return true;
+}
+
+OpenViBE::uint64 CSinusSignalGenerator::getClockFrequency(void) 
+{
+	return ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, m_ui32GeneratedEpochSampleCount);
 }
 
 void CSinusSignalGenerator::writeSignalOutput(const void* pBuffer, const EBML::uint64 ui64BufferSize)

openvibe-plugins/samples/trunc/src/ovpCTimeSignalGenerator.cpp
@@ -4,6 +4,8 @@
 #include <cmath>
 #include <cstdlib>
 
+#include <openvibe/ovITimeArithmetics.h>
+
 using namespace OpenViBE;
 using namespace OpenViBE::Kernel;
 using namespace OpenViBE::Plugins;
@@ -99,10 +101,9 @@ boolean CTimeSignalGenerator::process(void)
 		m_pSignalOutputWriterHelper->writeBuffer(*m_pSignalOutputWriter);
 		m_ui32SentSampleCount+=m_ui32GeneratedEpochSampleCount;
 
-		uint64 l_ui64StartTime;
-		uint64 l_ui64EndTime;
-		l_ui64StartTime=(((uint64)(l_ui32SentSampleCount))<<32)/m_ui32SamplingFrequency;
-		l_ui64EndTime  =(((uint64)(m_ui32SentSampleCount))<<32)/m_ui32SamplingFrequency;
+		uint64 l_ui64StartTime = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, l_ui32SentSampleCount);
+		uint64 l_ui64EndTime = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, m_ui32SentSampleCount);
+
 		l_pDynamicBoxContext->markOutputAsReadyToSend(0, l_ui64StartTime, l_ui64EndTime);
 	}
 
@@ -112,4 +113,9 @@ boolean CTimeSignalGenerator::process(void)
 void CTimeSignalGenerator::writeSignalOutput(const void* pBuffer, const EBML::uint64 ui64BufferSize)
 {
 	appendOutputChunkData<0>(pBuffer, ui64BufferSize);
+}
+
+OpenViBE::uint64 CTimeSignalGenerator::getClockFrequency(void) 
+{
+	return ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, m_ui32GeneratedEpochSampleCount);
 }

openvibe-applications/acquisition-server/trunc/src/brainproducts-brainampseries/ovasCDriverBrainProductsBrainampSeries.cpp
@@ -23,6 +23,7 @@
 #if defined TARGET_OS_Windows
 
 #include <openvibe-toolkit/ovtk_all.h>
+#include <openvibe/ovITimeArithmetics.h>
 
 #include <system/Memory.h>
 #include <system/Time.h>
@@ -485,7 +486,10 @@ boolean CDriverBrainProductsBrainampSeries::loop(void)
 				m_ui16Marker=l_ui16Marker;
 				if(m_ui16Marker != 0)
 				{
-					l_oStimulationSet.appendStimulation(m_ui16Marker, (uint64(j)<<32)/(m_oHeader.getSamplingFrequency()*m_ui32DecimationFactor), 0);
+					;
+					l_oStimulationSet.appendStimulation(m_ui16Marker, 
+						ITimeArithmetics::sampleCountToTime(m_oHeader.getSamplingFrequency()*m_ui32DecimationFactor, uint64(j)), 
+						0);
 					m_rDriverContext.getLogManager() << LogLevel_Trace << "Got stim code " << m_ui16Marker << " at sample " << j << "\n";
 				}
 			}

openvibe-applications/acquisition-server/trunc/src/brainproducts-brainvisionrecorder/ovasCDriverBrainProductsBrainVisionRecorder.cpp
@@ -10,6 +10,8 @@
 #include <cstdlib>
 #include <cstring>
 
+#include <openvibe/ovITimeArithmetics.h>
+
 using namespace OpenViBEAcquisitionServer;
 using namespace OpenViBE;
 using namespace OpenViBE::Kernel;
@@ -288,7 +290,7 @@ boolean CDriverBrainProductsBrainVisionRecorder::loop(void)
 			char* pszDescription = pszType + strlen(pszType) + 1;
 
 			m_vStimulationIdentifier[i] = atoi(strtok (pszDescription,"S"));
-			m_vStimulationDate[i] = (((uint64)(m_pStructRDA_Marker->nPosition )) << 32) / m_oHeader.getSamplingFrequency();
+			m_vStimulationDate[i] = ITimeArithmetics::sampleCountToTime(m_oHeader.getSamplingFrequency(), m_pStructRDA_Marker->nPosition );
 			m_vStimulationSample[i] = m_pStructRDA_Marker->nPosition;
 			m_pStructRDA_Marker = (RDA_Marker*)((char*)m_pStructRDA_Marker + m_pStructRDA_Marker->nSize);
 		}

openvibe-applications/acquisition-server/trunc/src/emotiv-epoc/ovasCConfigurationEmotivEPOC.cpp
@@ -46,10 +46,10 @@ boolean CConfigurationEmotivEPOC::postConfigure(void)
 	if(m_bApplyConfiguration)
 	{
 		::GtkToggleButton* l_pCheckbuttonGyro = GTK_TOGGLE_BUTTON(gtk_builder_get_object(m_pBuilderConfigureInterface, "checkbutton_gyro"));
-		m_rUseGyroscope = ::gtk_toggle_button_get_active(l_pCheckbuttonGyro);
+		m_rUseGyroscope = ::gtk_toggle_button_get_active(l_pCheckbuttonGyro) ? true : false;
 
 		::GtkSpinButton* l_pSpinButtonUserID = GTK_SPIN_BUTTON(gtk_builder_get_object(m_pBuilderConfigureInterface, "spinbutton_userid"));
-		m_rUserID = gtk_spin_button_get_value(l_pSpinButtonUserID);
+		m_rUserID = (uint32)gtk_spin_button_get_value(l_pSpinButtonUserID);
 
 		::GtkFileChooser* l_pFileChooserButton = GTK_FILE_CHOOSER(gtk_builder_get_object(m_pBuilderConfigureInterface, "filechooserbutton"));
 		gchar * l_sDir = gtk_file_chooser_get_filename(l_pFileChooserButton);

commit 71345d9dea8e5f6d1103886cced6855e2891abec

openvibe-plugins:  * Fixed frequency-related problems introduced in r3597.
git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe/trunk@3654 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

openvibe-plugins/file-io/trunc/src/box-algorithms/file-reading-and-writing/bci2000reader/ovpCBoxAlgorithmBCI2000Reader.cpp
@@ -102,7 +102,8 @@ boolean CBoxAlgorithmBCI2000Reader::processClock(IMessageClock& rMessageClock)
 
 uint64 CBoxAlgorithmBCI2000Reader::getClockFrequency(void)
 {
-	return ITimeArithmetics::sampleCountToTime(m_ui32Rate, m_ui32SampleCountPerBuffer);
+	// Intentional parameter swap to get the frequency
+	return ITimeArithmetics::sampleCountToTime(m_ui32SampleCountPerBuffer, m_ui32Rate);
 }
 
 void CBoxAlgorithmBCI2000Reader::sendHeader(void)

openvibe-plugins/file-io/trunc/src/ovpCBCICompetitionIIIbReader.cpp
@@ -209,7 +209,8 @@ namespace OpenViBEPlugins
 							"Please consider adjusting the BCI Competition IIIb reader settings to correct this!\n";
 					}
 
-					m_ui64ClockFrequency = ITimeArithmetics::sampleCountToTime(m_ui32SamplingRate, m_ui32SamplesPerBuffer);
+					// Intentional parameter swap to get the frequency
+					m_ui64ClockFrequency = ITimeArithmetics::sampleCountToTime(m_ui32SamplesPerBuffer, m_ui32SamplingRate);
 				}
 
 			}

openvibe-plugins/file-io/trunc/src/ovpCGDFFileReader.cpp
@@ -462,7 +462,8 @@ boolean CGDFFileReader::readFileHeader()
 					<< "Buffer size was " << m_ui32SamplesPerBuffer << "\n";
 			}
 
-			m_ui64ClockFrequency = ITimeArithmetics::sampleCountToTime(m_pSignalDescription.m_ui32SamplingRate, m_ui32SamplesPerBuffer);
+			// Intentional parameter swap to get the frequency
+			m_ui64ClockFrequency = ITimeArithmetics::sampleCountToTime(m_ui32SamplesPerBuffer, m_pSignalDescription.m_ui32SamplingRate);
 		}
 
 		//Send the data to the output

openvibe-plugins/samples/trunc/src/ovpCSinusSignalGenerator.cpp
@@ -128,7 +128,8 @@ boolean CSinusSignalGenerator::process(void)
 
 OpenViBE::uint64 CSinusSignalGenerator::getClockFrequency(void) 
 {
-	return ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, m_ui32GeneratedEpochSampleCount);
+	// Intentional parameter swap to get the frequency
+	return ITimeArithmetics::sampleCountToTime(m_ui32GeneratedEpochSampleCount, m_ui32SamplingFrequency);
 }
 
 void CSinusSignalGenerator::writeSignalOutput(const void* pBuffer, const EBML::uint64 ui64BufferSize)

openvibe-plugins/samples/trunc/src/ovpCTimeSignalGenerator.cpp
@@ -117,5 +117,6 @@ void CTimeSignalGenerator::writeSignalOutput(const void* pBuffer, const EBML::ui
 
 OpenViBE::uint64 CTimeSignalGenerator::getClockFrequency(void) 
 {
-	return ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, m_ui32GeneratedEpochSampleCount);
+	// Intentional parameter swap to get the frequency
+	return ITimeArithmetics::sampleCountToTime(m_ui32GeneratedEpochSampleCount, m_ui32SamplingFrequency);
 }